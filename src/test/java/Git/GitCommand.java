87d31a197d99354371a8d2f61640402e5a8df1d6
vicky274 <DELL@DESKTOP-B0O3T20.(none)>
2020-07-06 21:02:47 +0530

    New changes

---
+++ src/test/java/Git/GitCommand.java
@@ -0,0 +1,41 @@
+package Git;
+
+public class GitCommand {
+
+	
+	
+	
+	//https://dzone.com/articles/top-20-git-commands-with-examples
+	
+/*	git checkout -- . : remove all changes happened in local(in all files)
+
+	To create a new branch named “feature_x” and switch to it using
+	$ git checkout -b feature_x
+
+	What is a rebase in Git?
+	In Git, the rebase command integrates changes from one branch into another. It is an alternative to the better known "merge" command.
+
+	Most visibly, rebase differs from merge by rewriting the commit history in order to produce a straight, linear succession of commits.
+
+	GIT REBASE
+	For example, consider a situation where the master branch has progressed since you started working on a feature branch. 
+	You want to get the latest updates to the master branch in your feature branch, but you want to keep your branch's history 
+	clean so it appears as if you've been working off the latest master branch.
+
+	##Create a new repository on the command line
+
+	touch README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git remote add origin git@github.com:alexpchin/<reponame>.git
+	git push -u origin master
+	
+	
+	git diff branch1_ branch_2 -to compare two branch
+	git diff master filepath - to compare file in two branch
+	
+	git remove rm origin - to remove origin*/
+	
+	
+}
